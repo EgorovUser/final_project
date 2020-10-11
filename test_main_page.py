19e484ff17eb3c1c3996c3e8b72fbcf0ac7fbfba
Artem Egorov <sparta7499@gmail.com>
2020-10-11 16:26:40 +0300

    Изменен вариант перехода на страницу авторизации

--- test_main_page.py
+++ test_main_page.py
@@ -4,20 +4,24 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+#@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser,link)
     page.open()
     page.go_to_login_page() 
 
+#@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 def test_guest_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    time.sleep(3)
+    login_page.should_be_login_page()
